3c01e4a5005500ad3ff2cf01c641a1461c435a1a
MINYUZA <sncr_crmn@naver.com>
2021-06-26 14:03:50 +0900

    Feat : CRUD 기능 구축 완료

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Blog
 # Create your views here.
 
@@ -11,3 +12,34 @@ def home(request):
 def detail(request, id):
     blog = Blog.objects.get(id=id)
     return render(request, 'detail.html', {'blog': blog})
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now()
+    new_blog.save()
+    return redirect('detail', new_blog.id)
+
+
+def update(request, id):
+    blog = Blog.objects.get(id=id)
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.writer = request.POST["writer"]
+        blog.body = request.POST["body"]
+        blog.save()
+        return redirect('detail', blog.id)
+    return render(request, 'update.html', {"blog": blog})
+
+
+def delete(request, id):
+    blog = Blog.objects.get(id=id)
+    blog.delete()
+    return redirect("home")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,4 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('<int:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('update/', update, name="update"),
+    path('update/<int:id>', update, name="update"),
+    path('delete/<int:id>', delete, name="delete")
 ]
